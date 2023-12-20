23a42ef3fdfa0f8c6f5dacacf91080ec736ada2d
Williamolssoon <113092522+Williamolssoon@users.noreply.github.com>
2023-12-20 08:32:38 +0100

    Fixat Färgerna

--- client/script.js
+++ client/script.js
@@ -8,7 +8,7 @@ function fetchData(){
             let html = `<ul class="w-3/4 my-3 mx-auto flex flex-wrap gap-2 justify-center">`;
             players.forEach((player) => {
                 html += `<li
-                class="bg-${player.teamcolor1}-200 basis-1/4 text-${player.teamcolor2}-900 p-2 rounded-md border-2 border-${player.teamcolor}-400 flex flex-col justify-between">
+                class="bg-${player.teamcolor1} basis-1/4 text-${player.teamcolor2} p-2 rounded-md border-2 border-${player.teamcolor2} flex flex-col justify-between">
     
                 <h3>${player.firstName} ${player.lastName}</h3>
                 <p>Lag: ${player.team}</p>
@@ -17,12 +17,12 @@ function fetchData(){
                   <button
                     class="rounded-md bg-white/50 p-1 text-sm"
                     <button
-                      class="border border-${player.teamcolor2}-300 hover:bg-white/100 
+                      class="border border-${player.teamcolor2} hover:bg-white/100 
                       rounded-md bg-white/50 p-1 text-sm mt-2" onclick="setCurrentPlayer(${player.id})">
                       Ändra
                   
                   </button>
-                  <button class="border border-${player.teamcolor2}-300 hover:bg-white/100 
+                  <button class="border border-${player.teamcolor2} hover:bg-white/100 
                   rounded-md bg-white/50 p-1 text-sm mt-2" onclick="deletePlayer(${player.id})">
                     Ta bort
                   </button>
