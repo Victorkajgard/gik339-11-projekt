fe5f75cc389bf9fec46d3ec84a9ccf298131611d
Victorkajgard <victorkajgard@gmail.com>
2024-01-02 13:25:53 +0100

    2/1

--- client/script.js
+++ client/script.js
@@ -9,7 +9,7 @@ function fetchData(){
             let html = `<ul class="w-3/4 my-3 mx-auto flex flex-wrap gap-2 justify-center">`;
             players.forEach((player) => {
                 html += `<li
-                class="bg-${player.teamcolor1} basis-1/4 text-${player.teamcolor2} p-2 rounded-md border-2 border-${player.teamcolor2} flex flex-col justify-between">
+                class="bg-${player.teamcolor1} min-w-32 basis-1/4 text-${player.teamcolor2} p-2 rounded-md border-2 border-${player.teamcolor2} flex flex-col justify-between">
     
                 <h3>${player.firstName} ${player.lastName}</h3>
                 <p>Lag: ${player.team}</p>
@@ -39,8 +39,27 @@ function fetchData(){
     });
 }
 
+function setCurrentPlayer(id) {
+  fetch(`${url}/${id}`)
+      .then(result => result.json())
+      .then((player) => {
+          userForm.firstName.value = player.firstName;
+          userForm.lastName.value = player.lastName;
+          userForm.team.value = `${player.team},${player.teamcolor1},${player.teamcolor2}`;
+          userForm.position.value = player.position;
+
+          localStorage.setItem("currentId", player.id);
+      })
+}
+
+
+
 function createDeleteModal(id){
-    html = `<div class="flex justify-center items-center h-screen">
+  fetch(`${url}/${id}`)
+  .then(result => result.json())
+  .then((player) => {
+     
+      html = `<div class="flex justify-center items-center h-screen">
     <div x-data="{ open: true }">
       <!-- Modal Overlay -->
       <div x-show="open" class="fixed inset-0 px-2 z-10 overflow-hidden flex items-center justify-center">
@@ -49,15 +68,16 @@ function createDeleteModal(id){
         <div x-show="open" x-transition:enter="transition-transform ease-out duration-300" x-transition:enter-start="transform scale-75" x-transition:enter-end="transform scale-100" x-transition:leave="transition-transform ease-in duration-300" x-transition:leave-start="transform scale-100" x-transition:leave-end="transform scale-75" class="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50">
           <!-- Modal Header -->
           <div class="bg-indigo-500 text-white px-4 py-2 flex justify-between">
-            <h2 class="text-lg font-semibold">Delete</h2>
+            <h2 class="text-lg font-semibold">Ta bort</h2>
           </div>
           <!-- Modal Body -->
           <div class="p-4 text-black">
-          <p>The player has been removed</p>
+          <p>Vill du ta bort spelaren ${player.firstName} ${player.lastName}?</p>
           </div>
           <!-- Modal Footer -->
           <div class="border-t px-4 py-2 flex justify-end">
-            <button x-on:click="deletePlayer(${id})" class="px-3 py-1 bg-indigo-500 text-white  rounded-md w-full sm:w-auto"> Accept </button>
+            <button x-on:click="deletePlayer(${id})" class="px-3 py-1 bg-indigo-500 text-white mr-1 rounded-md w-full sm:w-auto"> Ta bort </button>
+            <button x-on:click="open=false" class="px-3 py-1 bg-indigo-500 text-white  rounded-md w-full sm:w-auto"> Avbryt </button>
           </div>
         </div>
       </div>
@@ -65,21 +85,11 @@ function createDeleteModal(id){
   </div>`
 
   document.body.insertAdjacentHTML("beforeend",html);
-}
 
-function setCurrentPlayer(id) {
-    fetch(`${url}/${id}`)
-        .then(result => result.json())
-        .then((player) => {
-            userForm.firstName.value = player.firstName;
-            userForm.lastName.value = player.lastName;
-            userForm.team.value = `${player.team},${player.teamcolor1},${player.teamcolor2}`;
-            userForm.position.value = player.position;
-  
-            
+      localStorage.setItem("currentId", player.id);
+  })
 
-            localStorage.setItem("currentId", player.id);
-        })
+    
 }
 
 function deletePlayer(id) {
@@ -88,8 +98,50 @@ function deletePlayer(id) {
 }
 
 
-userForm.addEventListener("submit", handleSubmit);
+const button = document.getElementsByName("submitUserForm");
 
+button.addEventListener("click", createModal);
+
+//button.addEventListener("submit", handleSubmit);
+
+function createModal(){
+  const id = localStorage.getItem("currentId");
+  if(id) {
+    message = `Vill du ändra spelaren ${userForm.firstName.value} ${userForm.lastName.value}?`
+  } else {
+    message = `Vill du lägga till spelaren ${userForm.firstName.value} ${userForm.lastName.value}?`
+  }
+    
+
+  html = `<div class="flex justify-center items-center h-screen">
+  <div x-data="{ open: true }">
+    <!-- Modal Overlay -->
+    <div x-show="open" class="fixed inset-0 px-2 z-10 overflow-hidden flex items-center justify-center">
+      <div x-show="open" x-transition:enter="transition-opacity ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition-opacity ease-in duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <!-- Modal Content -->
+      <div x-show="open" x-transition:enter="transition-transform ease-out duration-300" x-transition:enter-start="transform scale-75" x-transition:enter-end="transform scale-100" x-transition:leave="transition-transform ease-in duration-300" x-transition:leave-start="transform scale-100" x-transition:leave-end="transform scale-75" class="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50">
+        <!-- Modal Header -->
+        <div class="bg-indigo-500 text-white px-4 py-2 flex justify-between">
+          <h2 class="text-lg font-semibold">Lägg till</h2>
+        </div>
+        <!-- Modal Body -->
+        <div class="p-4 text-black">
+        <p>${message}</p>
+        </div>
+        <!-- Modal Footer -->
+        <div class="border-t px-4 py-2 flex justify-end">
+          <button x-on:click="handleSubmit(event)" class="px-3 py-1 bg-indigo-500 text-white mr-1 rounded-md w-full sm:w-auto"> Godkänn </button>
+          <button x-on:click="open=false" class="px-3 py-1 bg-indigo-500 text-white  rounded-md w-full sm:w-auto"> Avbryt </button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>`
+
+document.body.insertAdjacentHTML("beforeend",html);
+
+}
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -106,7 +158,7 @@ function handleSubmit(e) {
     teamArray = hej.value.split(",");
     serverPlayerObject.firstName = userForm.firstName.value;
     serverPlayerObject.lastName = userForm.lastName.value;
-    serverPlayerObject.team = teamArray[0]
+    serverPlayerObject.team = teamArray[0];
     serverPlayerObject.position = userForm.position.value;
     serverPlayerObject.teamcolor1 = teamArray[1];
     serverPlayerObject.teamcolor2 = teamArray[2];
--- server/players.sql
+++ server/players.sql
@@ -10,14 +10,14 @@ CREATE TABLE IF NOT EXISTS players(
 );
 INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (1,'William','Nylander','Toronto Maple Leafs','Forward','blue-900','white');
 INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (2,'Leo','Carlsson','Anaheim Ducks','Center','orange-500','black');
-INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (3,'Moritz','Seider','Detroit Red Wings','Defensemen','red-700','white');
-INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (4,'Jake','Oettinger','Dallas Stars','Goalie','green-700','white');
+INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (3,'Moritz','Seider','Detroit Red Wings','Back','red-700','white');
+INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (4,'Jake','Oettinger','Dallas Stars','Målvakt','green-700','white');
 INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (5,'Connor','McDavid','Edmonton Oilers','Center','orange-500','blue-700');
 INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (6,'Elias','Lindholm','Calgary Flames','Center','red-700','yellow-300');
-INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (7,'Juuse','Saros','Nashville Predators','Goalie','yellow-300','blue-700');
+INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (7,'Juuse','Saros','Nashville Predators','Målvakt','yellow-300','blue-700');
 INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (8,'Sam','Reinhart','Florida Panthers', 'Forward','red-700','blue-700');
 INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (9,'Elias','Pettersson','Vancouver Canucks', 'Center','blue-950','green-600');
-INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (10,'Rasmus','Dahlin','Buffalo Sabres','Defensemen','blue-700','yellow-300');
+INSERT INTO players(id,firstName,lastName,team, position ,teamcolor1, teamcolor2) VALUES (10,'Rasmus','Dahlin','Buffalo Sabres','Back','blue-700','yellow-300');
 
 
 select * from players;
